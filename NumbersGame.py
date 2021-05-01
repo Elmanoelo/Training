e3ad453ce481d2019c8c3323d6e9e5299c86db09
Amir <Amirreza.Mehrani@gmail.com>
2021-05-01 14:05:39 +0430

    nothing

--- NumbersGame.py
+++ NumbersGame.py
@@ -2,15 +2,12 @@ def joda_joda(num):
     x = [int(d) for d in str(num)]
     return x
 
-def changed(a):
-
 def find_in_two_numbers(first,second):
     a = joda_joda(first)
     b = joda_joda(second)
     if len(a) == len(b):
         a = " "
         for i in range(len(a)):
-            if ()
             for j in range(len(b)):
                 if (a[i] == b[j]):
                     if i == j:
